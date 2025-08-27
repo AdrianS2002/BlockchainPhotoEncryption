6962ed226b52e0c55a5cf7ceeac38515752dd22a
AdrianS2002 <aschiop2002@yahoo.com>
2025-08-27 15:02:15 +0300

    added frontend setup

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  standalone:true,
+  imports: [],
+  template: './app.component.html',
+  styles: ['./app.component.css']
+})
+export class AppComponent {
+  
+}
